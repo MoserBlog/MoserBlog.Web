ffc5a2ada1bc625dcb81525e65f6ebfb43cf322f
Philipp Mos <account@philipp-moser.de>
2022-06-18 12:55:00 -0700

    Setup Domain-Project ClassLibrary

---
+++ src/MoserBlog.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MoserBlog.Domain.Common;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime? CreateDate { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
