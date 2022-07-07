a894f91a64002ccb0cd637217e180e9b9c03486f
Philipp Mos <account@philipp-moser.de>
2022-07-07 17:29:31 +0200

    Update Header and Footer to Components

---
+++ src/MoserBlog.Web/Pages/Components/PageFooter/PageFooter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoserBlog.Web.Pages.Components.PageFooter;
+
+[ViewComponent]
+public class PageFooter : ViewComponent
+{
+    public IViewComponentResult Invoke() => View(nameof(PageFooter));
+}
