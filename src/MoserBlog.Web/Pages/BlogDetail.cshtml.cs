ee1b926649d60d6337d9039d491e3cba0a08f22d
Philipp Mos <account@philipp-moser.de>
2022-07-07 21:09:33 +0200

    Setup BlogDetail Page

---
+++ src/MoserBlog.Web/Pages/BlogDetail.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoserBlog.Web.Pages;
+
+public class BlogDetailModel : PageModel
+{
+    [ViewData]
+    public string PageTitle { get; set; }
+
+    private readonly ILogger<BlogDetailModel> _logger;
+
+    public BlogDetailModel(ILogger<BlogDetailModel> logger)
+    {
+        PageTitle = "Detail";
+
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+}
