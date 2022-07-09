6216b463130c7d3e08e93b981d596d8a2ccd079b
Philipp Mos <account@philipp-moser.de>
2022-07-09 19:54:29 +0200

    Add some dynamic values to BlogDetailPages

--- src/MoserBlog.Web/Services/Interfaces/IBlogService.cs
+++ src/MoserBlog.Web/Services/Interfaces/IBlogService.cs
@@ -1,8 +1,10 @@
-﻿using MoserBlog.Application.Features.BlogEntries.Queries.GetBlogEntryOverview;
+﻿using MoserBlog.Application.Features.BlogEntries.Queries.GetBlogEntryDetail;
+using MoserBlog.Application.Features.BlogEntries.Queries.GetBlogEntryOverview;
 
 namespace MoserBlog.Web.Services.Interfaces;
 
 public interface IBlogService
 {
     Task<List<BlogEntryOverviewVm>> GetBlogEntriesForOverviewAsync();
+    Task<BlogEntryDetailVm> TryGetBlogEntryByUrlName(string urLName);
 }
