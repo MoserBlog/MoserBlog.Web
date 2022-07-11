675820ccc715abb2cb9ed0991bf54ce4110088ba
Philipp Mos <account@philipp-moser.de>
2022-07-11 21:51:28 +0200

    Add support for building web-client

--- src/MoserBlog.Web/webpack.config.js
+++ src/MoserBlog.Web/webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -14,7 +16,51 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
             }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
         ]
     },
     plugins: [
@@ -23,9 +69,12 @@ module.exports = {
               { from: "node_modules/@philippmos/moserblog-styles/dist" }
             ]
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].bundle.min.css'
+        })
     ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
-    },
+    }
 }
